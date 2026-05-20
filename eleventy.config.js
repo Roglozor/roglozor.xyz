4c05f1316296a30d3b3cf24ff142926578c67214
Roglozor <me@roglozor.xyz>
2026-05-20 04:30:27 +0100

    Added RSS feed and Jabbit home page.

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,7 +3,12 @@ export default async function(eleventyConfig) {
   const assets = "./_src/assets/";
   eleventyConfig.addPassthroughCopy(assets);
   eleventyConfig.addWatchTarget(assets);
-
+  const pgp = "./_src/pgp.txt";
+  eleventyConfig.addPassthroughCopy(pgp);
+  eleventyConfig.addWatchTarget(pgp);
+  const rss = "./_src/rss.xml";
+  eleventyConfig.addPassthroughCopy(rss);
+  eleventyConfig.addWatchTarget(rss);
 };
 
 export const config = {
